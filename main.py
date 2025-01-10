60e087ce4a216947af6f4bc65e6fa2ee69cd3e96
Maschart <schwarz.martin1000@gmail.com>
2025-01-10 16:56:57 +0100

    count Characters

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-route = "/home/martin/workspace/github.com/Maschart/bookbot/books/frankenstein.txt"
+def main():
+    book_path = "/home/martin/workspace/github.com/Maschart/bookbot/books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
 
-with open(route) as f:
-    file_contents = f.read()
 
-print(file_contents)
+def get_num_words(text):
+    word = text.split()
+    return len(word)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return  f.read()
+
+
+main()
