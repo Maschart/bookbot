e6e16a53ae2bd2afb0bf28772f26f55d9b3911e7
Maschart <schwarz.martin1000@gmail.com>
2025-01-11 09:28:55 +0100

    lesson finished

--- main.py
+++ main.py
@@ -3,7 +3,17 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
-    print(chars_dict)
+    chars_sorted_list= chars_dict_to_sorted_list(chars_dict)
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The {item["char"]} character was found {item["num"]} times")
+
+    print("------End report------")
 
 
 def get_num_words(text):
@@ -11,6 +21,20 @@ def get_num_words(text):
     return len(word)
 
 
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(chars):
+    sort_lst = []
+    for ch in chars:
+        sort_lst.append({"char": ch, "num": chars[ch]})
+    sort_lst.sort(reverse=True, key=sort_on)
+    return sort_lst
+
+
+#wird in ein dictonary einsortiert und gezählt
 def get_chars_dict(text):
     chars = {}
     for c in text:
@@ -20,14 +44,10 @@ def get_chars_dict(text):
         else:
             chars[lowered] = 1
     return chars
-
-
+    
 def get_book_text(path):
     with open(path) as f:
         return  f.read()
-
-
-
 
 
 main()
